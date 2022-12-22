0265d6e0dc3157b047da537575931074f68330fc
Nicholas Siciliano <nsicilia@oswego.edu>
2022-12-22 16:18:55 -0500

    added extensions for time

--- Cassetta/Utils/Extensions.swift
+++ Cassetta/Utils/Extensions.swift
@@ -83,3 +83,29 @@ extension AVAudioSessionPortDescription {
         return portType == AVAudioSession.Port.headphones
     }
 }
+
+
+extension DateComponentsFormatter {
+    
+    static let abbreviated: DateComponentsFormatter = {
+        print("Initializing DateComponentsFormatter.abbreviated")
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.hour, .minute, .second]
+        formatter.unitsStyle = .abbreviated
+        
+        return formatter
+    }()
+    
+    
+    static let positional: DateComponentsFormatter = {
+        print("Initializing DateComponentsFormatter.abbreviated")
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.minute, .second]
+        formatter.unitsStyle = .positional
+        formatter.zeroFormattingBehavior = .pad
+        
+        return formatter
+    }()
+}
