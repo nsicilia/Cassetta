1dc27cac0c4b7081b8295deffd5d502f562cc7ed
Nicholas Siciliano <nsicilia@oswego.edu>
2023-02-05 23:37:09 -0500

    removed BlurView

--- Cassetta/View/Player/BlurView.swift
+++ Cassetta/View/Player/BlurView.swift
@@ -8,13 +8,13 @@
 import Foundation
 import SwiftUI
 
-struct BlurView: UIViewRepresentable {
-    var style: UIBlurEffect.Style
-    func makeUIView(context: Context) -> some UIVisualEffectView {
-        return UIVisualEffectView(effect: UIBlurEffect(style: style))
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        
-    }
-}
+//struct BlurView: UIViewRepresentable {
+//    var style: UIBlurEffect.Style
+//    func makeUIView(context: Context) -> some UIVisualEffectView {
+//        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+//    }
+//
+//    func updateUIView(_ uiView: UIViewType, context: Context) {
+//
+//    }
+//}
